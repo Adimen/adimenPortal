f1966c8c14c6f63f44b9064de4d9b1eaaf819cfb
Cesar Avila Frias <cafavila@gmail.com>
2020-03-08 11:24:25 -0300

    Cambio a pagina estatica base

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+var compression = require('compression')
 var express = require('express')
 var moment = require('moment')
+var https = require('https')
+var http = require('http')
 var app = express()
 
-app.get('/', function(req, res){
+app.use('/public', express.static(__dirname + '/public'))
+//app.use(compression)
+
+app.get('/about', function(req, res){
+    console.log('About')
     res.send('ADIMEN SPA')
 })
 
-var port = 3001
+app.get('/', function(req, res){
+    console.log('Home')
+    res.redirect('/public/home.html')
+})
 
-app.listen(port, function(){
+var port = 3001
+var ports = 3401
+
+http.createServer(app).listen(port, function(){
     console.log('%s [INFO] portal de Adimen en estado RUNNING en el puerto %s', moment().format("YYYY/MM/DD hh:mm:ss"), port);
+})
+
+https.createServer(app).listen(ports, function(){
+    console.log('%s [INFO] portal de Adimen en estado RUNNING en el puerto seguro %s', moment().format("YYYY/MM/DD hh:mm:ss"), ports);
 })
